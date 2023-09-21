d905db9c8cc883b45f8275f3a25106d3e7e7f204
danielplebani <danielpleba03@gmail.com>
2023-09-21 15:48:23 +0200

    exercize
    Create a new project using Vite: help yourself with the slides to retrace the various installation steps as seen in class.
    Modify the App.vue component, which must use the api option, and then in the template print a hello Vite title on the page

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+  import AppTitle from './components/AppTitle.vue'
+
+  export default{
+    components: {
+    AppTitle
+    },
+
+    data(){
+      return{
+        msg: 'hello vite'
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{msg}}</h1>
+
+  <AppTitle></AppTitle>
+</template>
+
+<style>
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script>
+  export default{
+    data(){
+      return{
+        msg2: 'La mia prima app con Vite!'
+      }
+    }
+  }
+</script>
+
+<template>
+  <h3>{{msg2}}</h3>
+</template>
+
+<style>
+</style>
